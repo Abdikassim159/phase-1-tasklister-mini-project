76acc07cd729e4589623b0b37041a4528729f09e
abdi kassim <123456789+odin@users.noreply.github.com>
2026-02-16 09:37:31 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const taskText = input.value;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    form.reset();
+  });
 });
